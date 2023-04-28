adad236aff937454d6ae5f6bf6f3942d0715ac20
mandeep <bhalothiamandeep@gmail.com>
2023-04-28 13:02:52 +0530

    Added debounce time to avoid fast sync status change

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/common/Constants.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/common/Constants.kt
@@ -4,3 +4,6 @@ import kotlin.time.Duration.Companion.seconds
 
 // Recommended timeout for Android configuration changes to keep Kotlin Flow from restarting
 val ANDROID_STATE_FLOW_TIMEOUT = 5.seconds
+
+// Our sdk keep checking for new blocks in bg and for a very short time state changes. For better UX we are adding this debounce
+val TRANSFER_TAB_ENABLE_DEBOUNCE_TIMEOUT = 2.seconds
