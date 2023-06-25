445b9cc05c981869ee64f0489d0a4f6da618a5b2
nighthawk24 <nighthawk24@users.noreply.github.com>
2023-06-25 00:59:50 -0400

    Update version and links

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/common/Constants.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/common/Constants.kt
@@ -4,8 +4,8 @@ import kotlin.time.Duration.Companion.seconds
 
 // Recommended timeout for Android configuration changes to keep Kotlin Flow from restarting
 val ANDROID_STATE_FLOW_TIMEOUT = 5.seconds
-const val SIDE_SHIFT_AFFILIATE_LINK = "https://sideshift.ai/"
-const val STEALTH_HEALTH_AFFILIATE_LINK = "https://stealthex.io/"
+const val SIDE_SHIFT_AFFILIATE_LINK = "https://sideshift.ai/a/EqcQp4iUM"
+const val STEALTH_HEALTH_AFFILIATE_LINK = "https://stealthex.io/?ref=x80l5bu8wq"
 const val ZCASH_LEARN_MORE_LINK = "https://z.cash/the-basics/"
 const val ZEC_MAX_AMOUNT = 21000000
 const val AMOUNT_QUERY = "amount"
@@ -16,7 +16,7 @@ val SUCCESS_VIBRATION_PATTERN = arrayOf(0L, 200L, 100L, 100L, 800L).toLongArray(
 val WRONG_VIBRATION_PATTERN = arrayOf(0L, 50L, 100L, 50L, 100L).toLongArray()
 const val KEY_SYNC_NOTIFICATION = "constants.key_sync_notification"
 const val WORKER_TAG_SYNC_NOTIFICATION = "constants.tag_sync_notification"
-const val VIEW_SOURCE_URL = "https://github.com/nighthawk-apps/nighthawk-wallet-android"
+const val VIEW_SOURCE_URL = "https://github.com/nighthawk-apps/nighthawk-android-wallet"
 const val PRIVACY_POLICY_LINK = "https://nighthawkwallet.com/privacypolicy/"
-const val BLOCK_EXPLORER_URL_TESTNET = "https://explorer.testnet.z.cash/tx/" // append transaction id in the end
+const val BLOCK_EXPLORER_URL_TESTNET = "https://testnet.zcashblockexplorer.com/transactions/" // append transaction id in the end
 const val BLOCK_EXPLORER_URL_MAIN_NET = "https://zcashblockexplorer.com/transactions/" // append transaction id in the end
